b6b9358ad2a2eff0bf21c1bb05fad13b4bc81ed7
Peter Harpending <pharpend2@gmail.com>
2014-08-12 19:28:03 -0600

    modify site.hs until it compiles.

--- src/site.hs
+++ src/site.hs
@@ -21,12 +21,14 @@ main = hakyll $ do
     route idRoute
     compile copyFileCompiler
 
-  match "css/*" $ do
+  match "stylesheets/*" $ do
     route $ setExtension "css"
     compile sass
 
-  match (fromList ["about.rst", "contact.markdown"]) $ do
-    route $ setExtension "html"
+  match "pages/*" $ do
+    route $
+      gsubRoute "pages/" (const "") `composeRoutes`
+      setExtension "html"
     compile $ pandocCompiler
       >>= loadAndApplyTemplate "templates/default.html" defaultContext
       >>= relativizeUrls
@@ -34,44 +36,7 @@ main = hakyll $ do
   match "posts/*" $ do
     route $ setExtension "html"
     compile $ pandocCompiler
-      >>= loadAndApplyTemplate "templates/post.html"    postCtx
-      >>= loadAndApplyTemplate "templates/default.html" postCtx
+      >>= loadAndApplyTemplate "templates/default.html" defaultContext
       >>= relativizeUrls
 
-  create ["archive.html"] $ do
-    route idRoute
-    compile $ do
-      posts <- recentFirst =<< loadAll "posts/*"
-      let archiveCtx =
-            listField "posts" postCtx (return posts) `mappend`
-            constField "title" "Archives"      `mappend`
-            defaultContext
-
-      makeItem ""
-        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-        >>= relativizeUrls
-
-
-  match "index.html" $ do
-    route idRoute
-    compile $ do
-      posts <- recentFirst =<< loadAll "posts/*"
-      let indexCtx =
-            listField "posts" postCtx (return posts) `mappend`
-            constField "title" "Home"        `mappend`
-            defaultContext
-
-      getResourceBody
-        >>= applyAsTemplate indexCtx
-        >>= loadAndApplyTemplate "templates/default.html" indexCtx
-        >>= relativizeUrls
-
   match "templates/*" $ compile templateCompiler
-
-
---------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-  dateField "date" "%B %e, %Y" `mappend`
-  defaultContext
