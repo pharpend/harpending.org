be41bedddc2c6e3551af656cb498bb770798a97f
Peter Harpending <peter@harpending.org>
2016-06-18 17:20:49 -0600

    Start cleanup

--- res/replace.js
+++
@@ -1,7 +0,0 @@
-$('table').each(function(){
-  $(this).addClass("table");
-  $(this).wrap("<div class='panel panel-default'>");
-})
-$('img').each(function(){
-  $(this).addClass("img-responsive center-block");
-})
